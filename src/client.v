6f2affcb4cf16fa036e65a5dd5c42d13216a15d5
snavy <1337snavy@gmail.com>
2023-02-18 23:21:02 +0000

    client message replying

--- src/client.v
+++ src/client.v
@@ -27,3 +27,21 @@ fn (client &Client) channel_message_send(channel string, content string) !Messag
 	return client.rest.fetch_json_data[Message]('post', client.rest.api_endpoint() +
 		'/channels/${channel}/messages', 'application/json', json.encode(data))!
 }
+
+// Fixme: This is kind of ugly, maybe 'map[string]Any' will get to replace this one day
+struct MessageReplyRequest {
+	content string
+	message_reference MessageReference
+}
+
+fn (client &Client) channel_message_send_reply(channel string, message string, content string) !Message {
+	data := MessageReplyRequest{
+		content: content,
+		message_reference: MessageReference{
+			message: Snowflake(message),
+		}
+	}
+
+	return client.rest.fetch_json_data[Message]('post', client.rest.api_endpoint() +
+	'/channels/${channel}/messages', 'application/json', json.encode(data))!
+}
