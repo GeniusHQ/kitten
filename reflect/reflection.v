6ac4fbe0f237b11222a8569e9522e4f3665f15c9
snavy <1337snavy@gmail.com>
2023-02-22 15:14:30 +0000

    fix type check when deserializing and made to_map accept non-mutable refs

--- reflect/reflection.v
+++ reflect/reflection.v
@@ -3,15 +3,15 @@ module reflect
 import x.json2
 
 pub interface Mappable {
+	to_map() map[string]json2.Any
 mut:
 	from_map(v map[string]json2.Any)
-	to_map() map[string]json2.Any
 }
 
 pub fn deserialize[T](src string) !T {
 	mut res := T{}
 
-	$if res is Mappable {
+	$if T is Mappable {
 		data := json2.raw_decode(src)!
 		res.from_map(data.as_map())
 		return res
