8bc794a24415ec06941974b066cc41df35b13498
snavy <1337snavy@gmail.com>
2023-02-24 08:41:26 +0000

    added id field to user

--- rest/user.v
+++ rest/user.v
@@ -4,10 +4,22 @@ import x.json2
 
 [noinit]
 pub struct User {
+pub mut:
+	id string
 	// Todo: complete this
 }
 
 pub fn (mut u User) from_map(data map[string]json2.Any) {
+	for key, val in data {
+		match key {
+			'id' {
+				u.id = val.str()
+			}
+			else {
+				dump('unimplemented ${key}')
+			}
+		}
+	}
 }
 
 pub fn (mut u User) to_map() map[string]json2.Any {
