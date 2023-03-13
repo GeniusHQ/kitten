28e6c1d30f1b147be2456503a3e2ac1d995bb2f5
snavy <1337snavy@gmail.com>
2023-03-13 17:06:48 +0100

    updated readme and pong example

--- README.md
+++ README.md
@@ -49,6 +49,10 @@ import kitten
 import kitten.universe
 import kitten.discord.intents
 
+fn handle_ready(mut client kitten.Client) ! {
+    println('Ready')
+}
+
 // This will reply to Discord and Guilded "!ping" messages
 fn handle_message(mut client kitten.Client, message &universe.Message) ! {
 	if message.content.to_lower() == "!ping" {
@@ -57,11 +61,16 @@ fn handle_message(mut client kitten.Client, message &universe.Message) ! {
 }
 
 fn main() {
-	mut client := kitten.new_client(
-		os.getenv("DISCORD_TOKEN"),
-		intents.guild_messages | intents.message_content,
-		os.getenv("GUILDED_TOKEN"))
+	mut client := kitten.new_client()
+	
+	client.with_discord(
+	    os.getenv("DISCORD_TOKEN"),
+	    intents.guild_messages | intents.message_content)
+		
+	client.with_guilded(
+	    os.getenv("GUILDED_TOKEN"))
 
+    client.universe_on_ready(handle_ready)
 	client.universe_on_message_create(handle_message)
 
 	client.start()!
--- examples/pong/main.v
+++ examples/pong/main.v
@@ -4,35 +4,31 @@ import os
 
 import kitten
 import kitten.universe
-import kitten.discord.intents as discord_intents
-import kitten.discord.gateway as discord_gateway
+import kitten.discord.intents
 
-fn get_discord_intents() int {
-	mut r := discord_intents.@none
-
-	r |= discord_intents.guild_messages // Guild Messages
-	r |= discord_intents.message_content // Message Content
-
-	return r
+fn handle_ready(mut client kitten.Client) ! {
+    println('Ready')
 }
 
-fn handle_on_ready(mut client kitten.Client, event &discord_gateway.ReadyEvent) ! {
-	println('Ready')
-}
-
-fn handle_universe_on_message_create(mut client kitten.Client, message &universe.Message) ! {
-	if message.content.to_lower() == '!ping' {
-		client.universe_channel_message_send(message.platform, message.channel, 'pong')!
+// This will reply to Discord and Guilded "!ping" messages
+fn handle_message(mut client kitten.Client, message &universe.Message) ! {
+	if message.content.to_lower() == "!ping" {
+		client.universe_channel_message_send(message.platform, message.channel, "pong")!
 	}
 }
 
 fn main() {
-	mut client := kitten.new_client(
-		os.getenv('DISCORD_TOKEN'), get_discord_intents(),
-		os.getenv("GUILDED_TOKEN"))
+	mut client := kitten.new_client()
 
-	client.discord_on_ready(handle_on_ready)
-	client.universe_on_message_create(handle_universe_on_message_create)
+	client.with_discord(
+	    os.getenv("DISCORD_TOKEN"),
+	    intents.guild_messages | intents.message_content)
+
+	client.with_guilded(
+	    os.getenv("GUILDED_TOKEN"))
+
+    client.universe_on_ready(handle_ready)
+	client.universe_on_message_create(handle_message)
 
 	client.start()!
 	client.wait()!
